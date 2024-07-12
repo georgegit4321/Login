984ea0ba304bf8f78e23c7528ca803d6f2d7f6cc
George Sebastian <georgesebastian4321@gmail.com>
2024-07-12 13:14:04 +0530

    login

---
+++ app/src/main/java/com/example/login/Forgot.java
@@ -0,0 +1,52 @@
+package com.example.login;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Forgot extends AppCompatActivity {
+    EditText e1,e2;
+    AppCompatButton b1,b2;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_forgot);
+        e1=(EditText) findViewById(R.id.pass);
+        e2=(EditText) findViewById(R.id.cpass);
+        b1=(AppCompatButton) findViewById(R.id.reset);
+        b2=(AppCompatButton) findViewById(R.id.back);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String pass=e1.getText().toString();
+                    String cpass=e2.getText().toString();
+
+                    Toast.makeText(getApplicationContext(), "password is reset",Toast.LENGTH_SHORT).show();
+                }catch (Exception e){
+                    Toast.makeText(getApplicationContext(),"invalid",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
